921a88a53d6007307d7a711913c898cb6ba8930f
Zeke Sikelianos <zeke@sikelianos.com>
2024-12-17 13:12:32 -0800

    generate images with replicate

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import Replicate from 'replicate';
 
 const app = new Hono<{ Bindings: Env }>();
 
@@ -30,6 +31,30 @@ app.post('/rtc-connect', async (c) => {
 	return c.body(sdp, {
 		headers: {
 			'Content-Type': 'application/sdp',
+		},
+	});
+});
+
+
+app.post('/generate-image', async (c) => {
+	const replicate = new Replicate({auth: c.env.REPLICATE_API_TOKEN})
+	const model = 'black-forest-labs/flux-schnell'  
+	const prompt = await c.req.text()
+	const output = await replicate.run(model, {
+	  input: {
+		prompt,
+		image_format: 'webp',
+	  }
+	})
+	  
+	// Some image models return an array of output files, others just a single file.
+	const imageUrl = Array.isArray(output) ? output[0].url() : output.url()
+   
+	console.log({imageUrl})
+
+	return c.body(imageUrl, {
+		headers: {
+			'Content-Type': 'image/webp',
 		},
 	});
 });
